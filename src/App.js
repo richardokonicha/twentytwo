9f4e91f44279d5fa2bf15c9d3396cdfce92b1c1f
Richie <richardokonicha@gmail.com>
2022-01-22 15:38:49 +0100

    add name text

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { Box, CssBaseline } from '@mui/material'
+
 
 function App() {
+  const [mousePos, setMousePos] = useState({ x: 0, y: 0 });
+
+  const theme = createTheme();
+
+  theme.typography.h3 = {
+    fontSize: '1.2rem',
+    '@media (min-width:600px)': {
+      fontSize: '1.5rem',
+    },
+    [theme.breakpoints.up('md')]: {
+      fontSize: '3.4rem',
+    },
+  };
+
+
+  useEffect(() => {
+    window.addEventListener('mousemove', (e) => {
+      let value = {
+        x: e.clientX,
+        y: e.clientY
+      }
+      console.log(value);
+      setMousePos(value);
+    })
+  }
+    , []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Box
+        display="flex"
+        justifyContent={'center'}
+        alignItems={'center'}
+        height={'100vh'}
+        sx={{
+          // fontFamily: 'Raleway',
+          fontSize: '3.4rem',
+          // backgroundColor: `rgb(58, 214, ${mousePos.x})`,
+        }
+        }
+
+      >
+        {/* {mousePos.x} */}
+        Hi there, I am Richard Okonicha
+      </Box>
+    </ThemeProvider >
   );
 }
 
